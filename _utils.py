7d62cde14143c778db951146956a8e0499b60f9e
kerlskemilkmal-art <kerlskemilkmal@gmail.com>
2026-06-05 14:04:27 +0300

    Add files via upload

--- _utils.py
+++ _utils.py
@@ -1,36 +1,120 @@
-from __future__ import annotations
+# Adapted from https://stackoverflow.com/a/9558001/2536294
 
-import enum
-import typing as t
+import ast
+import functools
+import operator as op
+from dataclasses import dataclass
+
+from ._multiprocessing_helpers import mp
+
+if mp is not None:
+    from .externals.loky.process_executor import _ExceptionWithTraceback
 
 
-class Sentinel(enum.Enum):
-    """Enum used to define sentinel values.
+# supported operators
+operators = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+    ast.FloorDiv: op.floordiv,
+    ast.Mod: op.mod,
+    ast.Pow: op.pow,
+    ast.USub: op.neg,
+}
 
-    .. seealso::
 
-        `PEP 661 - Sentinel Values <https://peps.python.org/pep-0661/>`_.
+def eval_expr(expr):
+    """Somewhat safely evaluate an arithmetic expression.
+
+    >>> eval_expr('2*6')
+    12
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4) / (6 + -7)')
+    -161.0
+
+    Raises ValueError if the expression is invalid, too long
+    or its computation involves too large values.
     """
+    # Restrict the length of the expression to avoid potential Python crashes
+    # as per the documentation of ast.parse.
+    max_length = 30
+    if len(expr) > max_length:
+        raise ValueError(
+            f"Expression {expr[:max_length]!r}... is too long. "
+            f"Max length is {max_length}, got {len(expr)}."
+        )
+    try:
+        return eval_(ast.parse(expr, mode="eval").body)
+    except (TypeError, SyntaxError, OverflowError, KeyError) as e:
+        raise ValueError(
+            f"{expr!r} is not a valid or supported arithmetic expression."
+        ) from e
 
-    UNSET = object()
-    FLAG_NEEDS_VALUE = object()
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}.{self.name}"
+def limit(max_=None):
+    """Return decorator that limits allowed returned values."""
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            ret = func(*args, **kwargs)
+            try:
+                mag = abs(ret)
+            except TypeError:
+                pass  # not applicable
+            else:
+                if mag > max_:
+                    raise ValueError(
+                        f"Numeric literal {ret} is too large, max is {max_}."
+                    )
+            return ret
+
+        return wrapper
+
+    return decorator
 
 
-UNSET: t.Literal[Sentinel.UNSET] = Sentinel.UNSET
-"""Sentinel used to indicate that a value is not set."""
+@limit(max_=10**6)
+def eval_(node):
+    if isinstance(node, ast.Constant) and isinstance(node.value, (int, float)):
+        return node.value
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
 
-FLAG_NEEDS_VALUE: t.Literal[Sentinel.FLAG_NEEDS_VALUE] = Sentinel.FLAG_NEEDS_VALUE
-"""Sentinel used to indicate an option was passed as a flag without a
-value but is not a flag option.
 
-``Option.consume_value`` uses this to prompt or use the ``flag_value``.
-"""
+@dataclass(frozen=True)
+class _Sentinel:
+    """A sentinel to mark a parameter as not explicitly set"""
 
-T_UNSET: t.TypeAlias = t.Literal[Sentinel.UNSET]
-"""Type hint for the :data:`UNSET` sentinel value."""
+    default_value: object
 
-T_FLAG_NEEDS_VALUE: t.TypeAlias = t.Literal[Sentinel.FLAG_NEEDS_VALUE]
-"""Type hint for the :data:`FLAG_NEEDS_VALUE` sentinel value."""
+    def __repr__(self):
+        return f"default({self.default_value!r})"
+
+
+class _TracebackCapturingWrapper:
+    """Protect function call and return error with traceback."""
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, **kwargs):
+        try:
+            return self.func(**kwargs)
+        except BaseException as e:
+            return _ExceptionWithTraceback(e)
+
+
+def _retrieve_traceback_capturing_wrapped_call(out):
+    if isinstance(out, _ExceptionWithTraceback):
+        rebuild, args = out.__reduce__()
+        out = rebuild(*args)
+    if isinstance(out, BaseException):
+        raise out
+    return out
