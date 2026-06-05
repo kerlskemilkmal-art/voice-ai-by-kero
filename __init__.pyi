31c12d4eeb41aa6794baef1b4b72711851c8bcb9
kerlskemilkmal-art <kerlskemilkmal@gmail.com>
2026-06-05 14:24:26 +0300

    Add files via upload

--- __init__.pyi
+++ __init__.pyi
@@ -1,54 +1,20 @@
-from collections.abc import Callable
-import subprocess
-from typing import Any, IO, Literal, overload
+from .geo import (
+    AitoffAxes as AitoffAxes,
+    HammerAxes as HammerAxes,
+    LambertAxes as LambertAxes,
+    MollweideAxes as MollweideAxes,
+)
+from .polar import PolarAxes as PolarAxes
+from ..axes import Axes
 
-def set_font_settings_for_testing() -> None: ...
-def set_reproducibility_for_testing() -> None: ...
-def setup() -> None: ...
-@overload
-def subprocess_run_for_testing(
-    command: list[str],
-    env: dict[str, str] | None = ...,
-    timeout: float | None = ...,
-    stdout: int | IO[Any] | None = ...,
-    stderr: int | IO[Any] | None = ...,
-    check: bool = ...,
-    *,
-    text: Literal[True],
-    capture_output: bool = ...,
-) -> subprocess.CompletedProcess[str]: ...
-@overload
-def subprocess_run_for_testing(
-    command: list[str],
-    env: dict[str, str] | None = ...,
-    timeout: float | None = ...,
-    stdout: int | IO[Any] | None = ...,
-    stderr: int | IO[Any] | None = ...,
-    check: bool = ...,
-    text: Literal[False] = ...,
-    capture_output: bool = ...,
-) -> subprocess.CompletedProcess[bytes]: ...
-@overload
-def subprocess_run_for_testing(
-    command: list[str],
-    env: dict[str, str] | None = ...,
-    timeout: float | None = ...,
-    stdout: int | IO[Any] | None = ...,
-    stderr: int | IO[Any] | None = ...,
-    check: bool = ...,
-    text: bool = ...,
-    capture_output: bool = ...,
-) -> subprocess.CompletedProcess[bytes] | subprocess.CompletedProcess[str]: ...
-def subprocess_run_helper(
-    func: Callable[[], None],
-    *args: Any,
-    timeout: float,
-    extra_env: dict[str, str] | None = ...,
-) -> subprocess.CompletedProcess[str]: ...
-def _check_for_pgf(texsystem: str) -> bool: ...
-def _has_tex_package(package: str) -> bool: ...
-def ipython_in_subprocess(
-    requested_backend_or_gui_framework: str,
-    all_expected_backends: dict[tuple[int, int], str],
-) -> None: ...
-def is_ci_environment() -> bool: ...
+class ProjectionRegistry:
+    def __init__(self) -> None: ...
+    def register(self, *projections: type[Axes]) -> None: ...
+    def get_projection_class(self, name: str) -> type[Axes]: ...
+    def get_projection_names(self) -> list[str]: ...
+
+projection_registry: ProjectionRegistry
+
+def register_projection(cls: type[Axes]) -> None: ...
+def get_projection_class(projection: str | None = ...) -> type[Axes]: ...
+def get_projection_names() -> list[str]: ...
